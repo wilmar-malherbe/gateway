799384422fa5e6557614dc01a04cdbed0fb7a850
Wilmar Malherbe <72525351+wilmar-malherbe@users.noreply.github.com>
2026-03-03 11:27:00 +0200

    Updated icon-symbol.tsx

--- components/ui/icon-symbol.tsx
+++ components/ui/icon-symbol.tsx
@@ -18,6 +18,9 @@ const MAPPING = {
   'paperplane.fill': 'send',
   'chevron.left.forwardslash.chevron.right': 'code',
   'chevron.right': 'chevron-right',
+  'newspaper.fill': 'newspaper',
+  'play.rectangle.fill': 'play-circle',
+  'ellipsis.circle.fill': 'more-horiz',
 } as IconMapping;
 
 /**
